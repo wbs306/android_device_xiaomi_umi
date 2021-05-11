d95c21406f4b67260e4e08261f484308748d36da
Ramii Ahmed <ramy@ahmedramy.com>
2021-05-11 06:33:35 +0000

    apollo: enable content based surface flinger refresh rate setting

--- device.mk
+++ device.mk
@@ -22,6 +22,8 @@ TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_SHIPPING_API_LEVEL := 29
+# Properties
+TARGET_VENDOR_PROP := $(LOCAL_PATH)/vendor.prop
 
 # Inherit from xiaomi sm8250-common
 $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
